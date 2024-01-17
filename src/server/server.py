3ed5aad8893c879fa1801be96c1c363a93895ee0
Utsav Basu <utsavbasu@outlook.com>
2024-01-17 17:03:44 +0530

    server running was on exposed net. fixed for now, need to change whie deploying

--- src/server/server.py
+++ src/server/server.py
@@ -3,6 +3,9 @@ from flask import Flask, Response, jsonify
 
 app = Flask(__name__)
 
+os.environ['SERVER_ID'] = '1234'
+
+
 @app.route('/home', methods=['GET'])
 async def home():
     """
@@ -12,10 +15,12 @@ async def home():
         message: Hello from Server: [ID]
         status: status of the response
     """
+
     return jsonify({
         'message': f"Hello from Server: {os.environ['SERVER_ID']}",
         'status': "successful"
     }), 200
+
 
 @app.route('/heartbeat', methods=['GET'])
 async def heartbeat():
@@ -26,7 +31,8 @@ async def heartbeat():
         message: [EMPTY]
         status: status of the response
     """
+
     return Response(status=200)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(port=8080, debug=True)
