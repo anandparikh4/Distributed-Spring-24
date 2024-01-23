f74dbde870dbfaebb5e06198505f19ea14b65500
Anamitra137 <anamitra137@gmail.com>
2024-01-24 00:41:40 +0530

    shifted to aiohttp for testing

--- src/client/tester.py
+++ src/client/tester.py
@@ -1,9 +1,11 @@
 import sys
 import signal
 import random
-import grequests
+# import grequests
 from matplotlib import pyplot as plt
 from pprint import pp
+import asyncio
+import aiohttp
 
 # %matplotlib inline
 
@@ -43,18 +45,66 @@ def other_request(
     return f'Response: {response.text}Code: {response.status_code}'
 # END other_request
 
+async def gather_with_concurrency(
+    session: aiohttp.ClientSession,
+    batch: int,
+    *urls: str
+):
+    """
+    Gather with concurrency from aiohttp async session
+    """
 
-def main():
+    semaphore = asyncio.Semaphore(batch)
 
-    requests = [grequests.get(f'{url}/home') for _ in range(500)]
-    responses = grequests.map(requests)
+    async def fetch(url: str):
+        async with semaphore:
+            async with session.get(url) as response:
+                await response.read()
+
+            await asyncio.sleep(0)
+
+            return response
+    # END fetch
+
+    tasks = [fetch(url) for url in urls]
+
+    return [None if isinstance(r, BaseException)
+            else r for r in
+            await asyncio.gather(*tasks, return_exceptions=True)]
+
+
+async def main():
+
+    # requests = [grequests.get(f'{url}/home') for _ in range(500)]
+    # responses = grequests.map(requests)
+
+    # N = 3
+    # counts = {k: 0 for k in range(N+1)}
+
+    # # for _, response in grequests.imap_enumerated(requests):
+    # for response in responses:
+    #     if response is None or not response.status_code == 200:
+    #         counts[0] += 1
+    #         continue
+
+    #     payload = response.json()
+    #     msg = payload['message']
+    #     server_id = int(msg.split(':')[-1].strip())
+    #     counts[server_id] += 1
+
+    # pp(counts)
+    # plt.bar(list(counts.keys()), list(counts.values()))
+    # plt.savefig('plot.jpg')
+
+    async with aiohttp.ClientSession() as session:
+        responses = await gather_with_concurrency(session, 100, *[f'{url}/home' for _ in range(100)])
 
     N = 3
     counts = {k: 0 for k in range(N+1)}
 
     # for _, response in grequests.imap_enumerated(requests):
     for response in responses:
-        if response is None or not response.status_code == 200:
+        if response is None or not response.status == 200:
             counts[0] += 1
             continue
 
@@ -71,4 +121,4 @@ def main():
 # END main
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
