a792ed64075fe00b9d17adfb0571b7cec25cd3ad
anamitra137 <anamitra137@gmail.com>
2024-03-09 18:11:27 +0530

    copy and read

---
+++ src/server/endpoints/database/copy.py
@@ -0,0 +1,78 @@
+from quart import Blueprint, current_app, jsonify, request
+from colorama import Fore, Style
+import sys
+
+from consts import *
+from common import *
+
+blueprint = Blueprint('copy', __name__)
+
+@blueprint.route('/copy', methods=['GET'])
+async def copy():
+    """
+        Returns all data entries corresponding to the requested shard tables in the server container
+
+        Request payload:
+            "shards": ["sh1", "sh2"...]
+            "admin": true/false
+        
+        Response payload:
+            "sh1": [data]
+            "sh2": [data]
+            ...
+            "status": "success"
+
+        Error payload:
+            "status": "error"
+            "message": "error message"
+            
+    """
+
+    try:
+        # Get the shard ids
+        payload = await request.get_json()
+        ic(payload)
+
+        shards = payload.get('shards', [])
+        admin = False
+        if payload.get('admin', '').lower() == 'true':
+            admin = True
+
+        response_payload = {}
+
+        # Get the data from the database
+        async with current_app.pool.acquire() as connection:
+            async with connection.transaction():
+                stmt = None
+                if admin:       # If admin, return data with shard_id and modified_at (Timestamp)
+                    stmt = connection.prepare('''
+                        SELECT * FROM StudT WHERE shard_id = ANY($1::varchar[]);
+                    ''')
+                else:           # If not admin, return only data
+                    stmt = connection.prepare('''
+                        SELECT stud_id, stud_name, stud_marks FROM StudT WHERE shard_id = ANY($1::varchar[]);
+                    ''')
+
+                async for record in stmt.cursor(shards):
+                    shard_id = record['shard_id']
+                    record = dict(record)
+                    if shard_id in response_payload:
+                        response_payload[shard_id].append(record)
+                    else:
+                        response_payload[shard_id] = [record]
+
+        response_payload['status'] = 'success'
+        return jsonify(ic(response_payload)), 200
+    
+    except Exception as e:
+        if DEBUG:
+            print(f'{Fore.RED}ERROR | '
+                  f'{e}'
+                  f'{Style.RESET_ALL}',
+                  file=sys.stderr)
+            
+        return jsonify(ic(err_payload(e))), 400     # Need to add err_playload() [to common.py ?]
+
+                    
+
+
--- src/server/server.py
+++ src/server/server.py
@@ -1,13 +1,49 @@
-import os
 import sys
 
-from icecream import ic
-from quart import Quart, Response, jsonify
+from quart import Quart
+from colorama import Fore, Style
 
 from consts import *
 from common import *
 
+from endpoints import blueprint as endpoints_blueprint
+
 app = Quart(__name__)
+
+@app.before_serving
+async def my_startup():
+    '''
+        Run startup tasks
+    '''
+
+    # Register blueprints
+    app.register_blueprint(endpoints_blueprint)
+
+    # Connect to the database
+    app.pool = await asyncpg.create_pool(  # type: ignore
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        host=DB_HOST,
+        port=DB_PORT
+    )
+
+    if app.pool is None:  # type: ignore
+        print(f'{Fore.RED}ERROR | '
+                f'Failed to connect to the database'
+                f'{Style.RESET_ALL}',
+                file=sys.stderr)
+        sys.exit(1)
+
+
+@app.after_serving
+async def my_shutdown():
+    '''
+        Run shutdown tasks
+    '''
+
+    # Close the database connection
+    await app.pool.close()
 
 
 
