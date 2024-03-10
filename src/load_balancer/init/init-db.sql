b8a33ef3adc458065898eb1ccee2c9b9dd3d46dd
Utsav Basu <utsavbasu@outlook.com>
2024-03-10 14:46:19 +0530

    init-db.sql

--- src/load_balancer/init/init-db.sql
+++ src/load_balancer/init/init-db.sql
@@ -1,8 +1,11 @@
+-- PostgreSQL 16.2
+-- This file is used to create the initial database schema
 START TRANSACTION;
 
+-- Create the Shard Metadata Table
 CREATE TABLE IF NOT EXISTS shardT (
 	stud_id_low INTEGER PRIMARY KEY,
-	shard_id TEXT NOT NULL,
+	shard_id TEXT NOT NULL UNIQUE,
 	shard_size INTEGER NOT NULL,
 	valid_at INTEGER NOT NULL DEFAULT 0
 );
