3c94741703a4a397a9b23a67cf6026d90d5f2145
Utsav Basu <utsavbasu@outlook.com>
2024-03-19 19:43:58 +0530

    generate data

---
+++ src/client/generate.py
@@ -0,0 +1,44 @@
+import csv
+import random
+from typing import List
+
+from attr import dataclass
+
+fnames: List[str] = []
+lnames: List[str] = []
+
+with open('data/fnames.txt', 'r') as file:
+    for line in file:
+        fnames.append(line.strip())
+
+with open('data/lnames.txt', 'r') as file:
+    for line in file:
+        lnames.append(line.strip())
+
+all_names = [f'{f} {l}' for f in fnames for l in lnames]
+
+@dataclass
+class Person:
+    id: int
+    name: str
+    marks: int
+
+
+def generate_people(n: int) -> List[Person]:
+    people = []
+    names = random.sample(all_names, n)
+    for i, name in zip(range(n), names):
+        marks = random.randint(0, 100)
+        people.append(Person(i, name, marks))
+    return people
+
+
+all_data = generate_people(4096*4)
+
+# Write to CSV
+with open('data/people.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerow(['id', 'name', 'marks'])
+    for person in all_data:
+        writer.writerow([person.id, person.name, person.marks])
+
