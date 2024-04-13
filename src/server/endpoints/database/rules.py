5a10ae129cc6ef5a7a9622ae863b63bbede19c95
Anand <anandparikh4@gmail.com>
2024-04-14 03:34:53 +0530

    bookkeeping rule done

--- src/server/endpoints/database/rules.py
+++ src/server/endpoints/database/rules.py
@@ -2,36 +2,133 @@ import common
 from common import *
 
 
-# All rules for enforcing synchronization between shard replicas across servers
-# Simply do 'await rules(shard_id, term)' in each operation. No return value
-async def rules(
+# Bookkeeping for enforcing synchronization between shard replicas across servers
+# Simply do 'await rules(shard_id, term, op)' in each op. No return value
+async def bookkeeping(
     shard_id: str,
-    valid_at: int
+    term: int,
+    op: str
 ):
     """
-        Rule 1 : Erase all entries where created_at > valid_idx or (deleted_at is not null and deleted_at <= valid_idx)
-        Rule 2 : Update deleted_at = null all entries where deleted_at > valid_idx
+        Rule:   If ((term > last_idx) || (term == last_idx && op == "r")){
+                    If(!executed) execute
+                }
+                Else If ((term < last_idx) || (term == last_idx && op != "r")){
+                    rollback
+                }
+    """
+
+    try:
+        async with common.pool.acquire() as conn:
+            async with conn.transaction():  # Enforce rule by executing database operations
+
+                # get the term from TermT
+                term_row = await conn.fetchrow('''--sql
+                    SELECT *
+                    FROM TermT
+                    WHERE shard_id = $1::TEXT;
+                ''',shard_id)
+
+                if term_row is None:
+                    raise Exception(f"Failed to enforce rule")
+                last_idx = term_row["last_idx"]
+                executed = term_row["executed"]
+
+                # last write/update/delete was successful was not executed on database, so execute now
+                if(((term > last_idx) or (term == last_idx and op == "r")) and executed == False):
+
+                    # update TermT executed to True
+                    await conn.execute('''--sql
+                        UPDATE TermT
+                        SET executed = TRUE
+                        WHERE shard_id = $1::TEXT                   
+                    ''',shard_id)
+
+                    # get the log from LogT                        
+                    log_row = await conn.fetchrow('''--sql
+                        SELECT *
+                        FROM LogT
+                        WHERE shard_id = $1::TEXT
+                        AND log_idx = $2::INTEGER
+                    ''',shard_id,last_idx)
+
+                    if log_row is None:
+                        raise Exception(f"Failed to enforce rule")
+                    operation = log_row["operation"]
+                    stud_id = log_row["stud_id"]
+                    content = log_row["content"]
+                    
+                    # do different things for different operations
+                    # write
+                    if(operation == "w"):
+                        stmt = await conn.prepare('''--sql
+                            INSERT INTO StudT (stud_id , stud_name , stud_marks , shard_id)
+                            VALUES ($1::INTEGER,
+                                    $2::TEXT,
+                                    $3::INTEGER,
+                                    $4::TEXT);
+                        ''')
+                        await stmt.executemany([(
+                            int(key),
+                            content[key][0],
+                            content[key][1],
+                            shard_id
+                        ) for key in content])
+                    # update
+                    elif(operation == "u"):
+                        await conn.execute('''--sql
+                            UPDATE StudT
+                            SET stud_name = $3::TEXT , stud_marks = $4::INTEGER
+                            WHERE shard_id = $1
+                            AND stud_id = $2
+                        ''',shard_id,stud_id,content[stud_id][0],content[stud_id][1])
+                    # delete
+                    elif(operation == "d"):
+                        await conn.execute('''--sql
+                            DELETE FROM StudT
+                            WHERE shard_id = $1
+                            AND stud_id = $2
+                        ''',shard_id,stud_id)
+                    # read
+                    else:   # operation == "r"
+                        pass
+
+                # last write/update/delete was not successful, so rollback
+                elif((term < last_idx) or (term == last_idx and op != "r")):
+                    
+                    # update TermT last_idx to last_idx-1 and executed to True
+                    await conn.execute('''--sql
+                        UPDATE TermT
+                        SET last_idx = $2::INTEGER , executed = TRUE
+                        WHERE shard_id = $1::TEXT
+                    ''',shard_id,last_idx-1)
+
+                    # delete last_idx log from LogT
+                    await conn.execute('''--sql
+                        DELETE FROM LogT
+                        WHERE shard_id = $1::TEXT
+                        AND log_idx = $2::INTEGER
+                    ''',shard_id,last_idx)
+
+    except Exception as e:
+        raise e
+
+
+# Service the request after bookkeeping
+async def service(
+    payload: dict,
+    operation: str
+):
+    """
+        Update TermT with new last_idx
+        Add new log entry in LogT
     """
 
     try:
         async with common.pool.acquire() as conn:
             async with conn.transaction():
-                # Enforce rules by executing database operations
-                await conn.execute('''--sql
-                    DELETE FROM StudT
-                    WHERE (shard_id = $1::TEXT)
-                        AND (created_at > $2::INTEGER
-                            OR (deleted_at IS NOT NULL
-                                AND deleted_at <= $2::INTEGER));
-                    ''', shard_id, valid_at)
-
-                await conn.execute('''--sql
-                    UPDATE StudT
-                    SET deleted_at = NULL
-                    WHERE shard_id = $1::TEXT
-                        AND deleted_at > $2::INTEGER;
-                    ''', shard_id, valid_at)
+                # decode payload based on the operation
+                pass
 
     except Exception as e:
-
         raise e
